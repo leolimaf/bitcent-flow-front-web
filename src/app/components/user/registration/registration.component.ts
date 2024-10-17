914d55f2af845781335cf47b9e0c4823a2a95d39
Leonardo Lima <leonardolimaf54@gmail.com>
2024-10-17 14:20:36 -0300

    feat: added form layout for user registration

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,11 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatCheckboxModule, MatButtonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  registrationForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.registrationForm = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      birthdate: [''],
+      phoneNumber: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: [''],
+      acceptTerms: [false],
+    });
+  }
+
+  onSubmit(){
+    console.log(this.registrationForm.value)
+  }
 }
