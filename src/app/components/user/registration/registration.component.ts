cc7d0fe2c360fc5131320ef60c02f5f49027a191
Leonardo Lima <leonardolimaf54@gmail.com>
2024-10-18 21:01:15 -0300

    feat: added some form validations

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatCheckboxModule, MatButtonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatCheckboxModule, MatButtonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
@@ -20,15 +21,30 @@ export class RegistrationComponent {
 
   constructor(private formBuilder: FormBuilder){
     this.registrationForm = this.formBuilder.group({
-      firstName: [''],
-      lastName: [''],
-      birthdate: [''],
-      phoneNumber: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-      acceptTerms: [false],
-    });
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      birthdate: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required, 
+        Validators.minLength(6), 
+        Validators.pattern(/(?=.*[^a-zA-Z0-9])/)]],
+      confirmPassword: ['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue],
+    },{validators:this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password')
+    const confirmPassword = control.get('confirmPassword')
+
+    if(password && confirmPassword && password.value !== confirmPassword)
+      confirmPassword?.setErrors({passwordMismatch:true})
+    else
+    confirmPassword?.setErrors(null)
+
+    return null;
   }
 
   onSubmit(){
