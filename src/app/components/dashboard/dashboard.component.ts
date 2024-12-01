8e023a6cfd7330e54dd4e6c1c156d653578f15dd
Leonardo Lima <leonardolimaf54@gmail.com>
2024-12-01 16:55:32 -0300

    feat: requisição a endpoint protegido + auth interceptor

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
+import { UserService } from '../../shared/services/user.service';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   
-  constructor(private router: Router, private authService: AuthService){}
+  constructor(private router: Router, private authService: AuthService, private userService: UserService){}
+
+  fullName: string = ''
+
+  ngOnInit(): void {
+    this.userService.getUserProfile().subscribe({
+      next:(res: any) => this.fullName = res.nomeCompleto,
+      error:(err: any) => console.log('Falha ao recuperar perfil do usuário:\n', err),
+    })
+  }
   
   onLogout(){
     this.authService.deleteToken();
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,22 +3,21 @@ import { Injectable } from '@angular/core';
 import { IUserRegistration } from '../../interfaces/user-registration.interface';
 import { IUserLogin } from '../../interfaces/user-login.interface';
 import { TOKEN_KEY } from '../constants';
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  baseUrl = 'http://localhost:5195/api';
-
   constructor(private http: HttpClient) { 
   }
 
   createUser(formData: IUserRegistration){
-    return this.http.post(this.baseUrl+'/signup', formData)
+    return this.http.post(environment.apiBaseUrl+'/signup', formData)
   }
 
   signin(formData: IUserLogin){
-    return this.http.post(this.baseUrl+'/signin', formData)
+    return this.http.post(environment.apiBaseUrl+'/signin', formData)
   }
 
   isLoggedIn(){
@@ -27,6 +26,10 @@ export class AuthService {
 
   saveToken(token: string){
     localStorage.setItem(TOKEN_KEY, token);
+  }
+
+  getToken(){
+    return localStorage.getItem(TOKEN_KEY);
   }
 
   deleteToken(){
